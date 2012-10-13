bc3534b32ec0ec950f02959cf1a6c8f8840d67cf
Yehuda B <yehudab@gmail.com>
2012-10-13 18:36:47 +0200

    IL site: Use latest known good graph in case the daily process failed

--- scripts/daily-process.sh
+++ scripts/daily-process.sh
@@ -38,11 +38,19 @@ else
         tail  "$logs/graph-builder-$today.log"| tee -a $logfile
         ls -l "$graphs/Graph.obj"| tee -a $logfile
         if [ ! -s "$graphs/Graph.obj" ] ; then
+            mv $graphs/Graph.obj "$graphs/Graph-$today-failed.obj"
             echo "Build of Graph.obj failed !!"| tee -a $logfile
+            echo "Using latest good graph:"| tee -a $logfile
+            ls -l $graphs/Graph-latest-good.obj | tee -a $logfile
         else
             mv $graphs/Graph.obj "$graphs/Graph-$today.obj"
-            ln -s  "$graphs/Graph-$today.obj" $graphs/Graph.obj 
+            if [ -h $graphs/Graph-latest-good.obj ] ; then
+                rm $graphs/Graph-latest-good.obj
+            fi
+            ln -s "$graphs/Graph-$today.obj" $graphs/Graph-latest-good.obj
+            echo "Build of Graph.obj succeeded" | tee -a $logfile
         fi
+        ln -s $graphs/Graph-latest-good.obj $graphs/Graph.obj
         echo Starting tomcat server| tee -a $logfile
         /sbin/service tomcat7 start
     fi
